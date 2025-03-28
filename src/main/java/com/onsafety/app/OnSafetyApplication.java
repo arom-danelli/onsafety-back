6bd41ec06c0477e9c57f82e2892c8b5640e4eb6d
arom-danelli <aromdanelli@hotmail.com>
2025-03-28 18:05:19 -0300

    initial commit

---
+++ src/main/java/com/onsafety/app/OnSafetyApplication.java
@@ -0,0 +1,13 @@
+package com.onsafety.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnSafetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onsafety/app/OnSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onsafety.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnSafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
